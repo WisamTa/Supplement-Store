cec9a09c70db0a8a07bcaa6d00d3a2d0adc63246
WisamTa <wisamtaqaly@gmail.com>
2022-08-20 17:38:31 +0000

    Working with checkout app/ stripe ...

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    """
+    A class for the checkout configuration
+    """
     name = 'checkout'
+
+    def ready(self):
+        """
+        import checkout signals library
+        """
+        import checkout.signals
