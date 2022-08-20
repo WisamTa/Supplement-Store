0df68f36d7068fb68bcd2c98755e65a893534575
WisamTa <wisamtaqaly@gmail.com>
2022-08-20 16:19:45 +0000

    Creating apps and urls

--- supplement_store/urls.py
+++ supplement_store/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
+    path('reviews/', include('reviews.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
