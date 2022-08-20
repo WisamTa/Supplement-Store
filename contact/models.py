2689b82c48e1da715b4f55e36127e0fdd7ac0603
WisamTa <wisamtaqaly@gmail.com>
2022-08-20 16:27:24 +0000

    Add models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    A class model for contact page
+    """
+    name = models.CharField(max_length=254, null=False, blank=False)
+    contact_email = models.EmailField(null=False, blank=False)
+    subject = models.CharField(max_length=254, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return str(self.name)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    A class for the category model
+    """
+    class Meta:
+        """
+        A class for Meta information
+        """
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the category name string
+        """
+        return str(self.name)
+
+    def get_friendly_name(self):
+        """
+        Returns the category friendly name string
+        """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    A class for the product model
+    """
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6,
+                                 decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
