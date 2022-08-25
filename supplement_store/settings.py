cae50b9cede64e14c2999a60d5a8ed0305e478c7
WisamTa <wisamtaqaly@gmail.com>
2022-08-25 12:55:26 +0000

    Add secret key

--- supplement_store/settings.py
+++ supplement_store/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '9_+bw@n#7k(^(!(sp6^o%s+$xzcr^*z*yr)+@$@2@!5ux+e1tb')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
