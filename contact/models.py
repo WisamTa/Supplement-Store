80fc21918d0612f6ba44622fcb4c5611edd1a85d
WisamTa <wisamtaqaly@gmail.com>
2022-08-28 20:12:40 +0000

    Trying to fix contact bug

--- contact/models.py
+++ contact/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
     """
     name = models.CharField(max_length=254, null=False, blank=False)
     contact_email = models.EmailField(null=False, blank=False)
-    subject = models.CharField(max_length=254, null=False, blank=False)
+    subject = models.TextField(max_length=254, null=False, blank=False)
     message = models.TextField(null=False, blank=False)
 
     def __str__(self):
